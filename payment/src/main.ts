2b130102e4ede6973cc1c2fc5a4de950fc528194
Tiago Lima <tiago.jlima.developer@gmail.com>
2023-07-05 21:33:30 -0300

    config

--- payment/src/main.ts
+++ payment/src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import kafka from 'kafka-node';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const client = new kafka.KafkaClient({ kafkaHost: 'localhost:29092' });
+  const producer = new kafka.Producer(client);
+  const KeyedMessage = kafka.KeyedMessage;
+  const km = new KeyedMessage('key', 'message');
+  const payloads = [
+    { topic: 'test', messages: 'hi', partition: 0 },
+    { topic: 'topic2', messages: ['hello', 'world', km] },
+  ];
+
+  producer.on('ready', function () {
+    console.log('Producer is ready');
+    producer.send(payloads, function (err, data) {
+      console.log(data);
+    });
+  });
+
+  producer.on('error', function (err) {
+    console.log('Producer is in error state');
+  });
+
   await app.listen(3001);
 }
 bootstrap();
